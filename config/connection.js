c6c61fd3504fc5c2da8f6a9e1d5af490ccb5941a
gman <dolivafigueroa@gmail.com>
2023-04-17 15:49:42 -0400

    update

--- config/connection.js
+++ config/connection.js
@@ -1,46 +1,21 @@
-// const Sequelize = require('sequelize');
-const mysql = require('mysql');
+const Sequelize = require('sequelize');
 require('dotenv').config();
-const connectionDetails = () => {
-  if (process.env.CLEARDB_DATABASE_URL) {
-    return `${process.env.CLEARDB_DATABASE_URL}`
-  }
-  return {
-    database: `${process.env.DB}`,
-    host: `${process.env.DB_HOST}`,
-    user: `${process.env.DB_USER}`,
-    password: `${process.env.DB_USER_PASSWORD}`,
-  };
-};
 
-// Create a connection to the database
-const connection = mysql.createConnection(connectionDetails());
+let sequelize;
 
-// open the MySQL connection
-connection.connect((error) => {
-  if (error) throw error;
-  // eslint-disable-next-line no-console
-  console.log(`Successfully connected to the database with ${connectionDetails()}.`);
-});
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
-module.exports = connection;
-
-
-// let sequelize;
-
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306
-//     }
-//   );
-// }
-
-// module.exports = sequelize;
+module.exports = sequelize;
